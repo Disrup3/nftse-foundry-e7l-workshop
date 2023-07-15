0bd065a0cb29b5799233459ae4236c629e947106
Alex Encinas <alexencmer@gmail.com>
2023-07-15 11:13:41 +0200

    initial commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// Import smart contracts that we are going to use 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
+    // FUNCTION EXECUTED BEFORE THE TESTES ARE EXECUTED
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
